01047c2557f3bd57e0355835c27856fbf070b437
tatianimeneghini <tatiani.meneghini@gmail.com>
2023-04-26 22:39:00 -0300

    feat: retornar valores de produtos disponíveis

--- webscraping.py
+++ webscraping.py
@@ -1,5 +1,6 @@
 from bs4 import BeautifulSoup
 import requests
+import re
 
 url = 'https://www.kabum.com.br/celular-smartphone/smartphones/iphone/iphone-14?page_number=1&page_size=100&facet_filters=eyJBcm1hemVuYW1lbnRvIjpbIjI1NiBHQiJdfQ==&sort=most_searched'
 
@@ -11,6 +12,14 @@ website = requests.get(url, headers=headers)
 soup = BeautifulSoup(website.content, 'html.parser')
 
 items = soup.select("div a.tEYzR", class_="productCard")
+prices = soup.select("span.hQOqhY", class_="priceCard")
 
+print("Os valores dos produtos disponíveis são: ")
+for price in prices:
+    if (price != "R$ ---"):
+        print(price.get_text())
+
+# print("------------------------")
+# print("Todos os produtos selecionados são: ")
 for item in items:
     print(item.text)
